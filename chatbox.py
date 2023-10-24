998fe54959d2ae5ce4fd5c7114279c7a8da90ac9
Bei Lin <129919638+lamberlin@users.noreply.github.com>
2023-10-24 11:54:06 -0700

    Update chatbox.py

--- chatbox.py
+++ chatbox.py
@@ -12,35 +12,25 @@ nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
 def predict_universities(new_review):
-    # Load the trained model
     model = Doc2Vec.load("my_model.doc2vec")
     uni_vectors = {df.iloc[i]['University']: model.docvecs[str(i)] for i in range(len(df))}
-
-    # Preprocess the new review
     nltk.download('punkt')
     nltk.download('stopwords')
     stop_words = set(stopwords.words('english'))
 
     words = word_tokenize(new_review.lower())
     new_review_words = [w for w in words if not w in stop_words]
-
-    # Infer the vector of the new review
     new_review_vector = model.infer_vector(new_review_words)
 
-    # Calculate the similarity of this review with each university's vector
-    # and store them in a dictionary
     uni_similarity = {}
-    for uni in uni_vectors.keys():  # assuming uni_vectors is your dictionary of university vectors
+    for uni in uni_vectors.keys():  
         vec = uni_vectors[uni]
         sim = cosine_similarity(new_review_vector.reshape(1, -1), vec.reshape(1, -1))[0][0]
         uni_similarity[uni] = sim
 
-    # Normalize the similarity scores so they sum to 1
     total_similarity = sum(uni_similarity.values())
     uni_probabilities = {uni: sim / total_similarity for uni, sim in uni_similarity.items()}
 
-    # Now, let's get the top 5 universities based on this similarity
     top_unis = sorted(uni_probabilities, key=uni_probabilities.get, reverse=True)
 
-    # Print the results
     return uni_probabilities
